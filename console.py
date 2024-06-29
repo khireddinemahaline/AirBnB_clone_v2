d74750ed0bf0489195e3dff2abec428cfcc0d0a7
khireddinemahaline <khirozr@gmail.com>
2024-06-29 09:38:51 -0400

    3task

--- console.py
+++ console.py
@@ -19,19 +19,38 @@ class HBNBCommand(cmd.Cmd):
     """HBNBCommand"""
     prompt = "(hbnb)"
 
-    def do_create(self, args):
-        """Create new instance"""
-        if len(args) < 2:
-            print('** class name missing **')
-        else:
-            try:
-                new = eval(args)()
-                print(new.id)
-                models.storage.save()
-            except (NameError, SyntaxError):
-                print("** class doesn't exist **")
-                pass
-        return
+    def do_create(self, arg):
+        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with given keys/values and print its id.
+        """
+        try:
+            if not arg:
+                raise SyntaxError()
+            my_list = arg.split(" ")
+
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
+                    value = value.strip('"').replace("_", " ")
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kwargs[key] = value
+
+            if kwargs == {}:
+                obj = eval(my_list[0])()
+            else:
+                obj = eval(my_list[0])(**kwargs)
+            models.storage.new(obj)
+            print(obj.id)
+            obj.save()
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
 
     def do_show(self, args):
         """string representation based on the class name and id"""
--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 
 
 import uuid
-import datetime
+from datetime import datetime
 import models
 
 
@@ -11,14 +11,22 @@ class BaseModel:
     """Base Model for creating and managing instances"""
     def __init__(self, *args, **kwargs):
         """ Constructor """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         models.storage.new(self)
 
     def save(self):
         """save atrrubuite instance"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def __str__(self):

650c929683ad7f3e5f062d4558f946f382f88703
khireddinemahaline <khirozr@gmail.com>
2024-06-29 10:27:52 -0400

    database dev

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,7 +4,7 @@ CREATE USER IF NOT EXISTS
     'hbnb_test'@'localhost'
     IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES 
-    ON hbnb_dev_db . *
+    ON hbnb_test_db . *
     TO 'hbnb_test'@'localhost';
 GRANT SELECT
     ON performance_schema . *

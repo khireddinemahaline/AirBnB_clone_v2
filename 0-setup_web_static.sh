57cfc0e71a7ca0281252bb5bd8f48136c75b1e4d
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-07-12 14:07:52 +0000

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
-
+sudo su
 apt-get -y install update
 apt-get -y install nginx
 sudo /etc/init.d/nginx start
@@ -10,6 +10,6 @@ mkdir -p /data/web_static/releases/test/
 echo "hello world" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/site-available/default
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
 exit 0

f143e3e18981ce8a0aa4f0cd4d80282370bac1e8
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-07-12 04:53:06 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,6 +10,6 @@ mkdir -p /data/web_static/releases/test/
 echo "hello world" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/site_available/default
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/site-available/default
 sudo /etc/init.d/nginx restart
 exit 0

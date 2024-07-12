3e128048a49e4dc951e0615d7aca38ef0968d635
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-07-12 14:20:59 +0000

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
-sudo su
-apt-get -y install update
-apt-get -y install nginx
+
+sudo apt-get -y install update
+sudo apt-get -y install nginx
 sudo /etc/init.d/nginx start
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo "hello world" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo echo "hello world" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
 exit 0

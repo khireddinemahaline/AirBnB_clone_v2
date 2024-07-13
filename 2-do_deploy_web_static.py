e1e3642ce2c6267e831b4b6aade36817a1261893
khireddinemahaline <khirozr@gmail.com>
2024-07-13 08:24:47 -0700

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,25 +7,32 @@ from datetime import datetime
 import shlex
 import os
 
-env.hosts = []
-env.user = ''
+env.hosts = ['54.89.58.11', '34.232.69.60']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """d""""
+    """d"""
     if not os.path.exists(archive_path):
-        Return False
+        return False
     try:
         filename = os.path.basename(archive_path)
         web_static_folder = filename.split('.')[0]
-
+        # realese path and tmp path
         release_path = '/data/web_static/releases/{}'.format(web_static_folder)
         temp_path = '/tmp/{}'.format(filename)
-
-
+        # put archive file into tmp folder
         put(archive_path, temp_path)
         run('mkdir -p {}'.format(release_path))
         run('tar -xzf {} -C {}'.format(temp_path, release_path))
-
-
-        run('rm {}'.format())
+        # delet tmp folder
+        run('rm -r {}'.format(temp_path))
+        run('mv {}/web_static/* {}'.format(release_path, release_path))
+        run('rm -rf {}/web_static'.format(release_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(release_path))
+        print('New version deployed!')
+        return True
+    except Exception as e:
+        print('Error: {}'.format(e))
+        return False

a66dad3e5aeb3cfbb0f015a0d5e07a37ecd4801f
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-07-13 14:29:34 +0000

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,12 @@
 #!usr/bin/env python3
 """Fabric script generates .tgz archive of all in web_static"""
 from fabric.api import local, task
-from time import strtime
+from datetime import datetime
 
 @task
 def do_pack():
     """generate .tgz archive file with name 'versions' """
-    timenow = strtime("%Y%M%W%D%H%M%S")
+    timenow = datetime.now().strftime('%Y%m%d%H%M%S')
     try:
         local("mkdir -p versions")
         filename = "versions/web_static_.tgz{}".format(timenow)

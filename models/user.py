b1155611b813c25a70b2e45690ad6b800eda0286
khireddinemahaline <khirozr@gmail.com>
2024-07-27 04:44:11 -0400

    orm

--- models/user.py
+++ models/user.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 """User Class"""
 
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
-from models.base_model import BaseModel
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """User Class"""
-    email: str = ''
-    password: str = ''
-    first_name: str = ''
-    last_name: str = ''
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+    else:
+        email: str = ''
+        password: str = ''
+        first_name: str = ''
+        last_name: str = ''

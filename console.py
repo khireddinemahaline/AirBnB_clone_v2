26311bdff4276927f1a27f47a7249627fe4596ae
khireddinemahaline <khirozr@gmail.com>
2024-08-03 11:02:27 -0400

    fix

--- console.py
+++ console.py
@@ -3,6 +3,11 @@
 ----to start the file : ./console.py"""
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 import cmd
 import models
 import shlex

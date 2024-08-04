5a8ec12cdbd43e4828dd20493b173f98975a2e86
khireddinemahaline <khirozr@gmail.com>
2024-08-04 02:19:22 -0700

    final version

--- models/place.py
+++ models/place.py
@@ -8,8 +8,10 @@ from sqlalchemy import Column, String, Float, Integer, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
 place_amenity = Table("place_amenity", Base.metadata,
-        Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-        Column('amenity_id', String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False))
+        Column('place_id', String(60), ForeignKey('places.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'), primary_key=True),
+        Column('amenity_id', String(60), ForeignKey("amenities.id", onupdate='CASCADE',
+                                            ondelete='CASCADE'), primary_key=True))
 
 
 class Place(BaseModel, Base):

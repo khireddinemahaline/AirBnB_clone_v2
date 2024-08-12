8e0370308fe2c8e33e7b13ac8488843c2dbd15f1
khireddinemahaline <khirozr@gmail.com>
2024-08-12 07:44:51 -0400

    a

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,6 +2,7 @@
 """return hello HBNB using flask"""
 
 from flask import Flask, render_template
+from markupsafe import escape
 from models import storage
 from models.state import State
 
@@ -10,9 +11,10 @@ app = Flask(__name__)
 
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def handel():
-    return render_template('7-states_list.html', states=storage.all(State).values())
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
 
 @app.teardown_appcontext
 def teardown(exec):

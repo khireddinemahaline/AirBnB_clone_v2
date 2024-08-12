5884d64001335b759323ffe4aee08eca170ad89e
khireddinemahaline <khirozr@gmail.com>
2024-08-12 09:18:27 -0400

    add

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-"""Return a list of states using Flask"""
+"""
+A simple Flask web application that displays a list of states from a database.
+
+Routes:
+    /states_list: Displays an HTML page with a list of states from the database.
+
+The application uses the Flask framework and SQLAlchemy for ORM.
+It connects to a database to fetch state data and render it in a template.
+"""
 
 from flask import Flask, render_template
 from models import storage
@@ -9,13 +17,26 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def handle():
-    """Handle the /states_list route"""
+    """
+    Handle the /states_list route.
+    
+    Retrieves all State objects from the database and renders them in an HTML template.
+    The states are displayed in alphabetical order by name.
+
+    Returns:
+        str: Rendered HTML page with a list of states.
+    """
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 @app.teardown_appcontext
 def teardown(exception):
-    """Close the storage session after each request"""
+    """
+    Close the SQLAlchemy session after each request.
+
+    Args:
+        exception (Exception): Exception that was raised during the request, if any.
+    """
     storage.close()
 
 if __name__ == '__main__':

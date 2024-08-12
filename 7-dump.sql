739fd138016bbce563c0e5bcd04890dc7cf4377b
khireddinemahaline <khirozr@gmail.com>
2024-08-12 05:57:01 -0400

    task 7 flask

--- 7-dump.sql
+++ 7-dump.sql
@@ -18,12 +18,16 @@
 -- Drop database
 DROP DATABASE IF EXISTS hbnb_dev_db;
 
--- Create database + user if doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS
+    'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES 
+    ON hbnb_dev_db . *
+    TO 'hbnb_dev'@'localhost';
+GRANT SELECT
+    ON performance_schema . *
+    TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 
 USE hbnb_dev_db;
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,13 +13,13 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def handel():
-    states = storage.all("State").values()
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 @app.teardown_appcontext
-def handel_teardown():
+def teardown(exec):
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
